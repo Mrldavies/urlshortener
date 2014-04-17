a4333e1700bf5278d20c219260f59bdc03190aa2
mrldavies <Mrldavies@ntlworld.com>
2014-04-17 16:11:36 +0100

    SQL file

---
+++ datebase.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` text NOT NULL,
+  `short` varchar(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
